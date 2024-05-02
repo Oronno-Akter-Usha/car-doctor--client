16a049985c2e58f0cee9954cea4f4032eb9ea8ac
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-05-03 01:11:33 +0600

    added navbar banner footer

---
+++ src/pages/Home/About/About.jsx
@@ -0,0 +1,41 @@
+import person from "../../../assets/images/about_us/person.jpg";
+import parts from "../../../assets/images/about_us/parts.jpg";
+
+const About = () => {
+  return (
+    <div className="hero mb-32">
+      <div className="hero-content flex-col lg:flex-row ">
+        <div className="lg:w-1/2 relative">
+          <img
+            src={person}
+            className="w-3/4 rounded-lg shadow-2xl h-[473px] "
+          />
+          <img
+            src={parts}
+            className="h-[300px] w-1/2 absolute right-5 -bottom-20 rounded-lg  shadow-2xl brightness-75"
+          />
+        </div>
+        <div className="lg:w-1/2 space-y-5">
+          <h3 className="text-[#FF3811] font-bold">About Us</h3>
+          <h1 className="text-4xl font-bold">
+            We are qualified & of experience in this field
+          </h1>
+          <p className="py-6">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which do not look even slightly
+            believable.
+          </p>
+          <p>
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which do not look even slightly
+            believable.
+          </p>
+          <button className="btn btn-error text-white">Get More Info</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,17 @@
+const Services = () => {
+  return (
+    <div>
+      <div className="text-center space-y-5">
+        <h3 className="text-2xl font-bold text-[#FF3811]">Services</h3>
+        <h2 className="text-5xl">Our Services Area</h2>
+        <p className="md:w-3/4 mx-auto">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which do not look even slightly
+          believable.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
